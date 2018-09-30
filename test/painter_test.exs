3dd41df9bec5d4a643f656fb09581673a80d7fb3
aleph-naught2tog <maggie@8thlight.com>
2018-09-30 10:46:19 -0500

    Sketches out first tests

--- test/painter_test.exs
+++ test/painter_test.exs
@@ -2,6 +2,22 @@ defmodule PainterTest do
   use ExUnit.Case
   doctest Painter
 
+  describe "idempotency" do
+    test "should return message unchanged"
+  end
+
+  @tag :skip
+  describe "handling types" do
+    test "should handle binaries gracefully"
+    test "should handle non-binaries gracefully"
+    test "should not add extra strings"
+  end
+
+  @tag :skip
+  describe "compatibility" do
+    test "should not use colors if ANSI not enabled"
+  end
+
   test "tests work" do
     assert true
   end
