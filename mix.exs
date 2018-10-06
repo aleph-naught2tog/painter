f45f047180c0b06e2fddd8b168098da53200fc72
aleph-naught2tog <maggie@8thlight.com>
2018-10-06 12:05:27 -0500

    Updates dependencies

--- mix.exs
+++ mix.exs
@@ -7,24 +7,41 @@ defmodule Painter.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        flags: [
+          :race_conditions,
+          :underspecs,
+          :unmatched_returns,
+          :overspecs,
+          :specdiffs,
+          :error_handling
+        ]
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {
+        :credo,
+        "~> 0.10.0",
+        only: [:dev, :test],
+        runtime: false
+      },
+      {
+        :dialyxir,
+        # git: "https://github.com/aleph-naught2tog/dialyxir.git",
+        path: "../dialyxir",
+        only: [:dev],
+        runtime: false
+      }
     ]
   end
 end
