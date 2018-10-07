a1984c0002d4ace9f49d842f38b13e74cd9c4806
aleph-naught2tog <maggie@8thlight.com>
2018-10-07 13:33:59 -0500

    with comments

--- lib/painter.ex
+++ lib/painter.ex
@@ -1,120 +1,155 @@
+defmodule Painter.Opts do
+  defstruct [
+    color: :default_color,
+    with_defaults: true,
+    name: nil,
+  ]
+end
+
 defmodule Painter do
-  @callback paint_color :: atom
-  @callback paint_name :: binary
+  @callback __color__::atom
+  @callback __name__::binary
+
+  @color &Painter.__color__/0
+  @name &Painter.__name__/0
 
   @moduledoc """
   Documentation for Painter.
   """
-  def format(message, color, name, opts \\ [])
-  def format(message, color, name, opts) when is_binary(message) do
-    get_header(color, name, opts) <> message
-  end
+#  def format(message, color, name, opts \\ [])
+#  def format(message, color, name, opts) when is_binary(message) do
+#    get_header(color, name, opts) <> message
+#  end
+#
+#  def format(message, color, name, opts) do
+#    message
+#    |> inspect(pretty: true)
+#    |> format(color, name, opts)
+#  end
+#
+#  defp get_header(color, name, opts) do
+#    name
+#    |> do_log_meta(opts)
+#    |> do_color(color)
+#    |> do_opts(opts)
+#  end
+#
+#  defp do_opts(value, opts) do
+#    Enum.reduce(
+#      opts,
+#      value,
+#      fn
+#        (current, tuple_value) when is_tuple(tuple_value) ->
+#          handle_opt("~~#{current}", tuple_value)
+#        (_,_) -> "#{value}"
+#      end
+#    )
+#  end
+#
+#  defp handle_opt(header, {key, value}) do
+#    case key do
+#      :label -> "#{header} #{value}:"
+#      _ -> header
+#    end
+#  end
+#
+#  def safe_raise(error), do: raise error
+#
+#  defp do_log_meta(name, mode: mode) do
+#    "[#{name}:#{mode}] "
+#  end
+#
+#  defp do_log_meta(name, _) do
+#    "[#{name}] "
+#  end
+#
+#  defp do_color(string, color) do
+#    chroma = apply(IO.ANSI, color, [])
+#    reset = apply(IO.ANSI, :reset, [])
+#    chroma <> string <> reset
+#  end
+#
+#  def do_label(message, label) do
+#    "#{label}: " <> inspect(message)
+#  end
 
-  def format(message, color, name, opts) do
-    message
-    |> inspect(pretty: true)
-    |> format(color, name, opts)
-  end
+#  def log(message, opts \\ []) do
+#    maybe_label = Keyword.get(opts, :label)
+#    maybe_mode = Keyword.get(opts, :mode)
+#
+#    final_message =
+#      case maybe_label do
+#        nil -> message
+#        _ -> do_label(message, maybe_label)
+#      end
+#
+#    header =
+#      case maybe_mode do
+#        nil -> @name
+#        _ -> do_log_meta(@name, mode: maybe_mode)
+#      end
+#
+#    final_message
+#    |> Painter.format(@color, header, opts)
+#    |> IO.puts()
+#
+#    message
+#  end
 
-  defp get_header(color, name, opts) do
-    name
-    |> do_log_meta(opts)
-    |> do_color(color)
-    |> do_opts(opts)
-  end
+#  def debug(variable) do
+#    log(variable, mode: :debug)
+#  end
+#
+#  def debug(variable, opts) do
+#    log(variable, [opts | {:mode, :debug}])
+#  end
 
-  defp do_opts(value, opts) do
-    Enum.reduce(
-      opts,
-      value,
-      fn
-        (current, tuple_value) when is_tuple(tuple_value) ->
-          handle_opt("~~#{current}", tuple_value)
-        (_,_) -> "#{value}"
+  def log(message), do: "message"
+
+  defmodule Defaults do
+    defmacro __using__(_) do
+      quote do
+        import Painter
       end
-    )
-  end
-
-  defp handle_opt(header, {key, value}) do
-    case key do
-      :label -> "#{header} #{value}:"
-      _ -> header
     end
   end
 
-  defp if_pipe(value, condition_func, do: do_block), do: if_pipe(value, condition_func, do: do_block, else: fn ident -> ident end)
-  defp if_pipe(value, condition_func, do: do_block, else: else_block) do
-    if condition_func.(value) do
-      do_block.(value)
-    else
-      else_block.(value)
-    end
-  end
+  defmacro __using__(init_opts \\ [])
+  defmacro __using__(list_opts) do
+    IO.inspect(list_opts, label: "init_opts before parse")
 
-  defp do_log_meta(name, mode: mode) do
-    "[#{name}:#{mode}] "
-  end
+    init_opts =
+      unless Keyword.get(list_opts, :name) do
+        temp_opts = struct(Painter.Opts, list_opts)
+        %{temp_opts | name: Atom.to_string(__CALLER__.module)}
+      else
+        struct(Painter.Opts, list_opts)
+      end
 
-  defp do_log_meta(name, _) do
-    "[#{name}] "
-  end
+    chosen_color = init_opts.color
+    chosen_name = init_opts.name
+    with_defaults = init_opts.with_defaults
 
-  defp do_color(string, color) do
-    chroma = apply(IO.ANSI, color, [])
-    reset = apply(IO.ANSI, :reset, [])
-    chroma <> string <> reset
-  end
-
-  def log(message, opts \\ [])
-  def log(message, mode: mode) when is_atom(mode) do
-    message
-    |> Painter.format(paint_color(), paint_name(), mode: mode)
-    |> IO.puts()
-
-    message
-  end
-
-  def log(message, label: label) do
-    log("#{label}: " <> inspect(message))
-
-    message
-  end
-
-  def log(message, opts) do
-    message
-    |> Painter.format(paint_color(), paint_name(), opts)
-    |> IO.puts()
-
-    message
-  end
-
-  def debug(variable) do
-    log(variable, mode: :debug)
-  end
-
-  def debug(variable, opts) do
-    log(variable, [opts | {:mode, :debug}])
-  end
-
-  def paint_color(), do: :cyan
-  def paint_name(), do: "?"
-
-  defoverridable [log: 2, paint_color: 0, paint_name: 0]
-
-  defmacro __using__(color: color, name: name) do
     quote do
-      def safe_raise(error), do: raise(error)
+      @behaviour Painter
 
-      def log(message, opts \\ []) do
-        message
-        |> Painter.format(paint_color(), paint_name(), opts)
-        |> IO.puts()
+      unquote(if with_defaults do
+        quote do
+          use Painter.Defaults
+        end
+      end)
 
-        message
+      @impl true
+      def __color__ do
+        unquote(chosen_color)
       end
 
-      def paint_color, do: unquote(color)
-      def paint_name, do: unquote(name)
+      @impl true
+      def __name__ do
+        unquote(chosen_name)
+      end
+
+      defoverridable(Painter)
     end
   end
 end
--- test/painter_test.exs
+++ test/painter_test.exs
@@ -1,7 +1,12 @@
 defmodule Tester do
-  use Painter, color: :red, name: "Tester"
+  use Painter, color: :red
+
   def test_log do
     Tester.log("hello world")
+  end
+
+  def test_debug do
+    Tester.debug("apples")
   end
 end
 
@@ -13,72 +18,86 @@ defmodule PainterTest do
   import TestHelpers.ColorHelper
   import TestHelpers.LogHelper
 
-  import Tester, only: [test_log: 0]
+#  import Tester, only: [test_log: 0, test_debug: 0]
 
-  describe "basic usage" do
-    test "should have ansi" do
-      assert has_any_ansi(&test_log/0)
-    end
 
-    test "should write to stdin" do
-      assert capture_io(&test_log/0)
-    end
-
-    test "should not write to stderr" do
-      message = "octopus party"
-      send_return_message = fn ->
-        send(self(), {:return, Tester.log(message)})
-      end
-      meta_capture = fn ->
-        # asserting here is just an extra level of safe
-        assert capture_io(send_return_message)
-      end
-      assert capture_io(:stderr, meta_capture) === ""
-      assert_receive({:return, message})
-    end
-
-    test "safe_raise should raise" do
-      assert_raise(RuntimeError, fn -> Tester.safe_raise("apples") end)
+  describe "log" do
+    @describetag :log
+    test "log out" do
+      IO.inspect(Tester.__info__(:functions))
     end
   end
 
-  describe "idempotency" do
-    test "should return message unchanged" do
-      message = "hello world"
-      assert_log_result(message, &test_log/0)
-    end
-
-    test "labeled log should return message unchanged" do
-      message = "beep boop"
-      log_with_label = fn -> Tester.log(message, label: "some label") end
-      assert_log_result(message, log_with_label)
-    end
-  end
-
-  describe "handling types" do
-    test "should handle binaries gracefully" do
-      refute_raise(fn -> silent_log(fn -> Tester.log("pears") end) end)
-    end
-
-    test "should handle non-binaries gracefully" do
-      refute_raise(fn -> silent_log(fn -> Tester.log(%{orange: "oranges", apple: :ok}) end) end)
-    end
-
-    test "should handle nil gracefully" do
-      refute_raise(fn -> silent_log(fn -> Tester.log(nil) end) end)
-    end
-  end
-
-  describe "compatibility" do
-    @describetag :skip
-    test "should not use colors if ANSI not enabled"
-  end
-
-  test "assert only accepts true" do
-    assert true
-  end
-
-  test "refute only accepts false" do
-    refute false
-  end
+#  describe "Debug" do
+#    test "should log" do
+#      assert_log(&test_debug/0)
+#    end
+#  end
+#
+#  describe "basic usage" do
+#    test "should have ansi" do
+#      assert has_any_ansi(&test_log/0)
+#    end
+#
+#    test "should write to stdin" do
+#      assert capture_io(&test_log/0)
+#    end
+#
+#    test "should not write to stderr" do
+#      message = "octopus party"
+#      send_return_message = fn ->
+#        send(self(), {:return, Tester.log(message)})
+#      end
+#      meta_capture = fn ->
+#        # asserting here is just an extra level of safe
+#        assert capture_io(send_return_message)
+#      end
+#      assert capture_io(:stderr, meta_capture) === ""
+#      assert_receive({:return, message})
+#    end
+#
+#    test "safe_raise should raise" do
+#      assert_raise(RuntimeError, fn -> Tester.safe_raise("apples") end)
+#    end
+#  end
+#
+#  describe "idempotency" do
+#    test "should return message unchanged" do
+#      message = "hello world"
+#      assert_log_result(message, &test_log/0)
+#    end
+#
+#    test "labeled log should return message unchanged" do
+#      message = "beep boop"
+#      log_with_label = fn -> Tester.log(message, label: "some label") end
+#      assert_log_result(message, log_with_label)
+#    end
+#  end
+#
+#  describe "handling types" do
+#    test "should handle binaries gracefully" do
+#      refute_raise(fn -> silent_log(fn -> Tester.log("pears") end) end)
+#    end
+#
+#    test "should handle non-binaries gracefully" do
+#      refute_raise(fn -> silent_log(fn -> Tester.log(%{orange: "oranges", apple: :ok}) end) end)
+#    end
+#
+#    test "should handle nil gracefully" do
+#      refute_raise(fn -> silent_log(fn -> Tester.log(nil) end) end)
+#    end
+#  end
+#
+#  describe "compatibility" do
+#    @describetag :skip
+#    test "should not use colors if ANSI not enabled"
+#  end
+#
+#  test "assert only accepts true" do
+#    assert true
+#  end
+#
+#  test "refute only accepts false" do
+#    refute false
+#  end
 end
