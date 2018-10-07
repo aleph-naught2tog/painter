ca27e434ecfedcaa2f520b577702ad9b4a551e1e
aleph-naught2tog <maggie@8thlight.com>
2018-10-06 22:27:10 -0500

    Tweaking

--- lib/painter.ex
+++ lib/painter.ex
@@ -1,45 +1,74 @@
 defmodule Painter do
+  @callback log(message::any | binary) :: any
+
   @moduledoc """
   Documentation for Painter.
   """
-  @spec format(message::binary, color::atom, name::binary) :: binary
-  def format(message, color, name) when is_binary(message) do
-    get_header(color, name) <> message
+  def format(message, color, name, opts \\ [])
+  def format(message, color, name, opts) when is_binary(message) do
+    get_header(color, name, opts) <> message
   end
 
-  @spec format(message::any, color::atom, name::binary) :: binary
-  def format(message, color, name) do
+  def format(message, color, name, opts) do
     message
     |> inspect(pretty: true)
-    |> format(color, name)
+    |> format(color, name, opts)
   end
 
-  @spec get_header(color::atom, name::binary) :: binary
-  defp get_header(color, name) do
+  defp get_header(color, name, opts \\ [])
+  defp get_header(color, name, mode: mode) when is_atom(mode),
+    do: get_header(color, name, Atom.to_string(mode))
+  defp get_header(color, name, mode: mode) when is_binary(mode) do
+    get_header(color, "[#{name}:#{mode}]")
+  end
+  defp get_header(color, name, _opts) do
     color_start = apply(IO.ANSI, color, [])
     reset = IO.ANSI.reset()
-    color_start <> "[#{name}]" <> reset
+    color_start <> "[#{name}] " <> reset
   end
+
+  def log(message, opts \\ [])
+  def log(message, mode: mode) when is_atom(mode) do
+    message
+    |> Painter.format(:default_color, "[?]", mode: mode)
+    |> IO.puts()
+
+    message
+  end
+
+  def log(message, label: label) do
+    log("#{label}: " <> inspect(message))
+
+    message
+  end
+
+  def log(message, opts) do
+    message
+    |> Painter.format(:default_color, "[?]", opts)
+    |> IO.puts()
+
+    message
+  end
+
+  def debug(variable) do
+    quote do
+      unquote(variable)
+      |> Macro.decompose_call()
+      |> IO.inspect()
+      log(unquote(variable), mode: :debug)
+    end
+  end
+
+  defoverridable [log: 2]
 
   defmacro __using__(color: color, name: name) do
     quote do
       def safe_raise(error), do: raise(error)
 
-      @spec log(message::binary, label: message::binary) :: binary
-      def log(message, label: label), do: log(message, label)
-
-      @spec log(message::any) :: any
-      def log(message) do
+      def log(message, opts \\ []) do
         message
-        |> Painter.format(unquote(color), unquote(name))
+        |> Painter.format(unquote(color), unquote(name), opts)
         |> IO.puts()
-
-        message
-      end
-
-      @spec log(message::any, label::binary) :: any
-      def log(message, label) do
-        log("#{label}: " <> inspect(message))
 
         message
       end
