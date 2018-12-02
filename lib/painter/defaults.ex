47652c28a9630c60317c808762407af4ec9d5eb5
aleph-naught2tog <maggie@8thlight.com>
2018-12-02 10:31:53 -0600

    formatting

--- lib/painter/defaults.ex
+++ lib/painter/defaults.ex
@@ -2,27 +2,49 @@ defmodule Painter.Defaults do
   defmacro __using__(_) do
     caller = __CALLER__.module
 
+    # quote location: :keep do
     quote location: :keep do
-      defmacro detail(message, opts \\ []) do
-        import Printer, only: [line: 2, with_line_break: 2]
-        import Painter, only: [pretty: 3]
-
+      defmacro detail(raw_message, opts \\ []) do
         indent = 2
-        footer = pretty(__CALLER__, Printer.parse(message), indent)
+        footer = Printer.pretty(__CALLER__, raw_message, 2 * indent)
 
         quote do
-          prefix = with_line_break(String.duplicate(" ", unquote(indent)), :before)
-          suffix = unquote(footer) <> line(paint_color(), 80)
-          new_opts = Keyword.merge([suffix: suffix, prefix: prefix], unquote(opts))
-          log(unquote(message), new_opts)
+          footer_func = unquote(footer)
+          value = unquote(raw_message)
+          new_opts = Keyword.merge([suffix: footer_func], unquote(opts))
+          message = Printer.do_indent(value, Painter.Opts.default_opts(true), unquote(indent))
+
+          log(message, new_opts)
         end
       end
 
-      def write(message, opts \\ []), do: Painter.write(unquote(caller), message, opts)
-      def log(message, opts \\ []), do: Painter.log(unquote(caller), message, opts)
-      def debug(message, opts \\ []), do: Painter.debug(unquote(caller), message, opts)
-      def mark(message, opts \\ []), do: Painter.mark(unquote(caller), message, opts)
-      def error(message, opts \\ []), do: Painter.error(unquote(caller), message, opts)
+      def to_pretty(format_fun, message) do
+        format_fun.(message)
+      end
+
+      def write(message, opts \\ []) do
+        Painter.write(unquote(caller), message, opts)
+      end
+      
+      def log(message, opts \\ []) do
+        Painter.log(unquote(caller), message, opts)
+      end
+      
+      def debug(message, opts \\ []) do
+        Painter.debug(unquote(caller), message, opts)
+      end
+      
+      def mark(message, opts \\ []) do
+        Painter.mark(unquote(caller), message, opts)
+      end
+      
+      def error(message, opts \\ []) do
+        Painter.error(unquote(caller), message, opts)
+      end
+      
+      def fail(cause \\ :unknown, opts \\ []) do
+        Painter.fail(unquote(caller), cause, opts)
+      end
     end
   end
 end
